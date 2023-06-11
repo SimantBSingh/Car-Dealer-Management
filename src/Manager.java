4b09ab9abd9298794028bcbae30e9689b08263bd
sbsingh3_2121 <sbsingh3@uno.edu>
2023-06-11 02:59:27 -0500

    deleting unnecesarry files

--- src/Manager.java
+++
@@ -1,8 +0,0 @@
-public class Manager extends Person{
-
-    public Manager(String name, int age, String street, String city, int ZIPCode, String username, String password, String phoneNumber, String email, int id){
-        super(name, age, street, city, ZIPCode, username, password, phoneNumber, email);
-    }
-
-
-}
